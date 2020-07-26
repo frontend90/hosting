837dd97f08f62a21b5088198209b4d7b9dae90f0
Vasiliy Pupkin <vpupkin@company.com>
2020-07-26 19:52:32 +0300

    верстка

--- js/main.js
+++ js/main.js
@@ -122,3 +122,20 @@ $('.register-modal p a').click(function () {
 
   $('.register-modal').removeClass('show');
 });
+
+/*************       service-clients  slick       ************/
+$('.service-clients-slider').slick({
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: true,
+  dots: true,
+  autoplay: true,
+  speed: 1000,
+  responsive: [{
+    breakpoint: 801,
+    settings: {
+      dots: false
+    }
+  }]
+});
